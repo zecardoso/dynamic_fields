096d1d996d9b25677206c86b3cf4663d0d04dbd5
José Cardoso <zehcardoso@outlook.com>
2023-11-12 22:40:24 -0300

    creating GenerateProcedureCommand method

--- src/integration/database_integration.js
+++ src/integration/database_integration.js
@@ -1,9 +1,27 @@
-import { SqlServer } from "../library/sql_server.js"
-
 export class DataBaseIntegration {
-    Execute(source) {
-        const fields = SqlServer.Execute(source);
+    constructor() {
 
-        return fields;
+    }
+
+    Execute(procedure, parameters) {
+        const command = this.#GenerateProcedureCommand(procedure, parameters);
+
+        // const fields = SqlServer.Execute(command);
+    }
+
+    #GenerateProcedureCommand(procedure, parameters) {
+        var command = "exec ";
+
+        command = command.concat(procedure, " ")
+
+        parameters.forEach((parameter, index) => {
+            if (index !== 0) {
+                command = command.concat(", ")
+            }
+
+            command = command.concat("@", parameter.name, " = ", parameter.value)
+        });
+
+        return command;
     }
 }
