b8ff3082c15ab2d30520372132ff0226231ef091
José Cardoso <zehcardoso@outlook.com>
2023-11-19 21:13:14 -0300

    calling get form from service

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import { FieldConditionUserRepository } from "./repository/field_condition_user_repository.js"
+import { FormService } from "./service/form_service.js"
 import express from "express"
+
+const service = new FormService()
+const form = await service.Get(1)
+console.log(form)
 
 const app = express()
 
