3711ca0fef7d15d002eee3a94f50541fb5c1b11c
José Cardoso <zehcardoso@outlook.com>
2023-11-19 21:06:59 -0300

    returning recordSets or null

--- src/library/sql_server.js
+++ src/library/sql_server.js
@@ -31,14 +31,11 @@ export class SqlServer {
 
             await pool.close()
 
-            console.log(result.recordsets.length) // count of recordsets returned by the procedure
-            console.log(result.recordsets[0].length) // count of rows contained in first recordset
-            console.log(result.recordset) // first recordset from result.recordsets
-            console.log(result.returnValue) // procedure return value
-            console.log(result.output) // key/value collection of output values
-            console.log(result.rowsAffected) // array of numbers, each number represents the number of rows affected by executed statemens
+            if (result !== null && result.recordsets !== null && result.recordsets.length > 0) {
+                return result.recordsets;
+            }
 
-            return result;
+            return null;
         } catch (error) {
             console.error(error)
         }
