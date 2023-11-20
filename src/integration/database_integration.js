961641fe82439339bc3535fd44acca61f4c240bf
José Cardoso <zehcardoso@outlook.com>
2023-11-19 21:08:18 -0300

    using instance instead static

--- src/integration/database_integration.js
+++ src/integration/database_integration.js
@@ -1,10 +1,15 @@
 import { SqlServer } from "../library/sql_server.js"
 
 export class DataBaseIntegration {
+    #database
+
     constructor() {
+        this.#database = new SqlServer()
     }
 
-    Execute(procedure, parameters) {
-        const fields = SqlServer.Execute(procedure, parameters)
+    async Execute(procedure, parameters) {
+        const recordSets = await this.#database.Execute(procedure, parameters)
+
+        return recordSets;
     }
 }
